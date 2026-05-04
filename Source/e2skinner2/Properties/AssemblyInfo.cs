5d71fa98c043ff127aeb25c6ed92200a76eb9ddf
popking159 <47087407+popking159@users.noreply.github.com>
2026-05-04 17:36:12 +0300

    Add files via upload

--- Source/e2skinner2/Properties/AssemblyInfo.cs
+++ Source/e2skinner2/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Resources;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("4.2.0.0")]
-[assembly: AssemblyVersion("4.2.0.0")]
-[assembly: AssemblyFileVersion("4.2.0.0")]
+[assembly: AssemblyVersion("4.2.0.1")]
+[assembly: AssemblyFileVersion("4.2.0.1")]
 [assembly: NeutralResourcesLanguageAttribute ("en")]
